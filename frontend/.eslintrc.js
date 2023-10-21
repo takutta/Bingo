b9566b8cca217f81e5e4567c27cabba7c4aa4b7d
takutta <jaaxtra@gmail.com>
2023-10-21 11:52:12 +0300

    Disabled eslint no-alert rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prop-types': 0,
     'prettier/prettier': 'error',
+    'no-alert': 'off',
     'import/no-extraneous-dependencies': [
       'error',
       {
