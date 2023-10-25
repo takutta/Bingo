24f971dbf26ec4dab6dcae1e92275b1a32e07eb2
Jaakko Haavisto <jaaxtra@gmail.com>
2023-10-25 22:23:45 +0300

    removed prettier from eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'prettier'],
+  extends: ['plugin:react/recommended']
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
